cd070f93442cecdeedaeb30a01cc529c4d86f03f
makenziewortley <MEW020@student.bham.ac.uk>
2024-11-03 15:31:45 +0000

    Progress on exercise 3

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -3,48 +3,7 @@
 #include <string>
 #include <vector>
 
-std::string transformChar(const char in_char)
-{
-    std::string inputText;
-    if (std::isalpha(in_char)) {
-        inputText += std::toupper(in_char);
-        return inputText;
-        }
-    switch (in_char) {
-            case '0':
-                inputText += "ZERO";
-                break;
-            case '1':
-                inputText += "ONE";
-                break;
-            case '2':
-                inputText += "TWO";
-                break;
-            case '3':
-                inputText += "THREE";
-                break;
-            case '4':
-                inputText += "FOUR";
-                break;
-            case '5':
-                inputText += "FIVE";
-                break;
-            case '6':
-                inputText += "SIX";
-                break;
-            case '7':
-                inputText += "SEVEN";
-                break;
-            case '8':
-                inputText += "EIGHT";
-                break;
-            case '9':
-                inputText += "NINE";
-                break;
-        }
-
-        return inputText;
-}
+#include "TransformChar.hpp"
 
 bool processCommandline(const std::vector<std::string>& args, bool& helpRequested, bool& versionRequested, 
     std::string& inputFile, std::string& outputFile){
